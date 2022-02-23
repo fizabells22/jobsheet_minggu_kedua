5f5a274ac0cf84966fade510233653129bfa1d80
fizabells22 <fizabella.ru@gmail.com>
2022-02-24 00:57:55 +0700

    Basic Aplikasi Flutter

--- jobsheet_minggu_kedua/lib/main.dart
+++ jobsheet_minggu_kedua/lib/main.dart
@@ -16,19 +16,21 @@ class MyApp extends StatelessWidget {
       ),
       body : Column(
         mainAxisSize: MainAxisSize.min,
-        crossAxisAlignment: CrossAxisAlignment.center,
         children :[
         Row(
+          mainAxisAlignment: MainAxisAlignment.center,
         children : [ 
           Container(
           margin: const EdgeInsets.all(10),
+          alignment :Alignment.center,
           child : const Text('BERITA TERBARU', style: TextStyle(fontSize: 15, color: Colors.black)),
         ),
-        SizedBox(
+        const SizedBox(
           width: 30,
         ),
         Container(
           margin: const EdgeInsets.all(10),
+          alignment :Alignment.center,
           child : const Text('PERTANDINGAN HARI INI', style: TextStyle(fontSize: 15, color: Colors.black)),
         ),
           ],
@@ -68,15 +70,79 @@ class MyApp extends StatelessWidget {
                 Container(
                   margin : const EdgeInsets.only(top: 5),
                   padding : const EdgeInsets.only(bottom : 10),
-                  child : const Text('Transfer'),
+                  child : const Text('Transfer', style: TextStyle(fontSize: 15, color: Colors.black, fontWeight: FontWeight.w800),
+                )
                 )
               ]
-            )
+            ),
+              Row(
+                children: [
+                Container(
+                  decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border.all(
+                    ),
+                  ),
+                  child: 
+                  Row(
+                    children: [
+                      Column(
+                        children: [
+                          Container(
+                            height: 150,
+                            width: 200,
+                            decoration: const BoxDecoration(
+                            image: DecorationImage(
+                            image: NetworkImage("https://picsum.photos/seed/picsum/200"), fit: BoxFit.fitWidth,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(15.0),
+                        child: Column(
+                          children: [
+                          const Text("Pique Bilang Wasit Untungkan", style: TextStyle( fontSize: 15, fontWeight: FontWeight.w800,
+                                )),
+                          const Text("Madrid, Koeman Tepok Jidat", style: TextStyle( fontSize: 15, fontWeight: FontWeight.w800,
+                                )),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ]),
+              Row(
+                children: [
+                  Container(
+                      height: 50,
+                      width: 500,
+                      decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border.all(
+                        ),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Row(
+                          children: [
+                          const Text("Barcelona Feb 13, 2021",style: TextStyle( fontSize: 15, fontWeight: FontWeight.w800,
+                                )
+                                ),
+                          ],
+                        ),
+                      )
+                      ),
+                ],
+              ),
+            ],
+          ),
+        ),
         ],)
       )
-        ],)
-    )
-    );
+        );
   }
 }
 
